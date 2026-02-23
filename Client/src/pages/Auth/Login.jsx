d20ac0d5a1ae523ed4b7496b4951b3c15f06fbbc
GauravMorey <gaurav1234pmorey@gmail.com>
2026-02-23 21:10:12 +0530

    loggin page is almost completed

--- Client/src/pages/Auth/Login.jsx
+++ Client/src/pages/Auth/Login.jsx
@@ -1,10 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import AuthLayout from '../../components/layouts/AuthLayout'
+import { useNavigate } from 'react-router-dom';
+import Input from '../../components/Inputs/Input';
 
 const Login = () => {
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+  // Handle Login Form Submission
+  const handleLogin = async (e) => {
+    e.preventDefault();
+  }
   return (
-    <div>
-      login
-    </div>
+    <AuthLayout>
+      <div className='lg:w-[70%] h3/4 md:h-full flex flex-col justify-center'>
+        <h3 className='text-xl font-semibold text-black'>Welcome Back!</h3>
+        <p className='text-xs text-slate-700 mt-[5px] mb-6'>Please enter your detials to log in!</p>
+
+        <form onSubmit={handleLogin}>
+          <Input
+            value={email}
+            onChange={({ target }) => setEmail(target.value)}
+            lable="Email Address"
+            placeholder="gaurav@example.com"
+            type="text"
+          />
+
+        </form>
+
+      </div>
+    </AuthLayout>
   )
 }
 
